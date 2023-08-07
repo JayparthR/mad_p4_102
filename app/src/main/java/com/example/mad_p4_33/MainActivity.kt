f95744b7962b5942b13dcba0a9e2f7a6893ae8d5
student <student@gnu.ac.in>
2023-08-07 10:44:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_p4_33/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mad_p4_33
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var l:Button=findViewById(R.id.login);
+        
+    }
+}
