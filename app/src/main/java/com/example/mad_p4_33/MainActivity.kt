a3579b2b36564e5b1be2e99816eccad479fe8fba
jansaridhruvi <dhruvijansari21@gnu.ac.in>
2023-08-13 15:37:27 +0530

    mad_pract4_k1.1

--- app/src/main/java/com/example/mad_p4_33/MainActivity.kt
+++ app/src/main/java/com/example/mad_p4_33/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mad_p4_33
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var l:Button=findViewById(R.id.login);
-        
+        var s:Button=findViewById(R.id.Signup);
+        s.setOnClickListener(){
+            var i=Intent(this,reg::class.java)
+            startActivity(i);
+        }
     }
 }
